719430c486ed2af575bd0d7b8b0311b4d80fc0e5
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-07 17:29:35 +0530

    - added pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './styles/App.css';
+import HomePage from './pages/HomePage';
+import WorksPage from './pages/WorksPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/works" element={<WorksPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
     </Routes>
   );
 }
