9a459d0ae6cb341994d8c9aa87880e9d1af1ec36
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-07 17:25:09 +0530

    - added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+    </Routes>
   );
 }
 
