f021c7c4252243efa7fe157b23f833c18a1a5b0c
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-16 17:13:57 +0530

    edited styles

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Menu = () => {
     return (
-        <div>Menu</div>
+        <>
+            <div className="menu">
+                <Navbar />
+            </div>
+        </>
     )
 }
 
--- src/pages/WorksPage.js
+++ src/pages/WorksPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Heading from '../components/Heading'
 import Card from '../components/Card'
+import Menu from '../components/Menu'
 
 const WorksPage = () => {
     return (
@@ -21,11 +22,37 @@ const WorksPage = () => {
                                     desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'}
                                     link={'/'}
                                 />
+                                <Card
+                                    title={'Project Title'}
+                                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'}
+                                    link={'/'}
+                                />
+                                <Card
+                                    title={'Project Title'}
+                                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'}
+                                    link={'/'}
+                                />
+                                <Card
+                                    title={'Project Title'}
+                                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'}
+                                    link={'/'}
+                                />
+                                <Card
+                                    title={'Project Title'}
+                                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'}
+                                    link={'/'}
+                                />
+                                <Card
+                                    title={'Project Title'}
+                                    desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'}
+                                    link={'/'}
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Menu />
         </>
     )
 }
